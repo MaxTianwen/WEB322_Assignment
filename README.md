311a3f758101b9019e04ea13a34c1c63a4ef8c6e
天问 王 <wtw0816@163.com>
2024-09-19 21:36:10 -0400

    first commit for project

---
+++ README.md
@@ -0,0 +1,19 @@
+# Web322 Assignment
+
+Student Name: Tianwen Wang
+Student Number: 151583226
+Student Email: twang118@myseneca.ca
+Date Created: 2024/09/19
+
+GITHUB URL:  
+VERCEL URL:
+
+### Technology Stack
+
+**Frontend**:  
+**Backend**: TBD  
+**Database**: TBD
+
+### Notes
+
+By submitting this as my assignment, I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part of this assignment has been copied manually or electronically from any other source (including web sites) or distributed to other students.
---
+++ server.js
@@ -0,0 +1,25 @@
+// "require" the Express module
+const express = require('express');
+
+// Get the absolute path
+const path = require('path');
+
+// obtain the "app" object
+const app = express();
+
+// set up a port
+const HTTP_PORT = process.env.PORT || 3000; // Port number: 3000
+
+// GET Route for index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/index.html'));
+})
+
+// GET Route for about.html
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/about.html'));
+})
+
+// start the server on the port and output a confirmation to the console
+app.listen(HTTP_PORT, () => console.log(`server listening on: ${HTTP_PORT}`));
+
